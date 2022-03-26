544b991ad9b7a1df98676e76866bd0a1609d82c9
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-25 21:47:02 -0300

    Componente App faz o mesmo do exemplo HTML usando JSX

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     return (
-        <div>
-            Meu primeiro componente React.
+        <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+            <label htmlFor="nome" style={{display: 'block', marginBottom: 4}}>
+                Nome:
+            </label>
+            <input id="nome" type="text" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', width: '100%', boxSizing: 'border-box'}}/>
+            <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', borderRadius: 8, width: '100%'}}>Enviar</button>
         </div>
     )
 }
